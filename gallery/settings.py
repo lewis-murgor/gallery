5c9a0cad0801312ae54188919e9776cc48896be7
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-25 13:31:24 +0300

    modify settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h8#-hja7a9x@6j8zg8g3qt_a2ei6_jy=l4-rxx#8a44a_^bmq*'
+MODE = config("MODE", default="dev")
+SECRET_KEY = SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
