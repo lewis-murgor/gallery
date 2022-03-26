6b47b2032d0f9f45d63f01ebc7d6cc156d87072c
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-26 19:49:06 +0300

    modify search view function

--- myphotos/views.py
+++ myphotos/views.py
@@ -11,12 +11,12 @@ def images(request):
 def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_name(search_term)
-        message = f"{search_term}"
+        category = request.GET.get("image")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
 
         return render(request, 'search.html',{"message":message,"images": searched_images})
 
     else:
-        message = "You haven't searched for any name"
+        message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
