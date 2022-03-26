d98722785f5e372b55e71e6f5cbd701f0fe94013
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-26 14:21:52 +0300

    add model methods

--- myphotos/models.py
+++ myphotos/models.py
@@ -7,11 +7,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
 class Location(models.Model):
     location = models.CharField(max_length =30)
 
     def __str__(self):
         return self.location
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
 
 class Image(models.Model):
     image = models.ImageField(upload_to = 'photos/')
@@ -23,5 +35,29 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self):
+        self.update()
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image = cls.objects.filter(id = id)
+        return image
+
+    @classmethod
+    def search_by_category(cls,category):
+        image = cls.objects.filter(category=category)
+        return image
+
+    @classmethod
+    def filter_by_location(cls,location):
+        image = cls.objects.filter(location = location)
+        return location
 
 
