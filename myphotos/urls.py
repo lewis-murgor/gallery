fb6b9286f3bae99b7fd2a51e54f5c411c6d1561e
Lewis <lewis.murgor@student.moringaschool.com>
2022-03-26 20:56:05 +0300

    create image url

--- myphotos/urls.py
+++ myphotos/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     re_path('^$',views.images,name='images'),
     re_path(r'^search/', views.search_results, name='search_results'),
+    re_path(r'^image/(\d+)',views.image,name ='image')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
